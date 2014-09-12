3a89690704c5e676aceadf7c3adfc46fa8b01897
rsgoss <rsgoss@comcast.net>
2014-09-12 13:00:12 -0400

    more work plus uas

--- js/reveal.js
+++ js/reveal.js
@@ -20,7 +20,7 @@ var Reveal = (function(){
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
 			width: 1280,
-			height: 768,
+			height: 1024,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.001,
